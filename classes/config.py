75767a1442fada9f3a87072abb10b87e66d223e0
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-10-06 01:58:25 +0200

    fix: path of config file

--- classes/config.py
+++ classes/config.py
@@ -4,11 +4,13 @@ import os
 
 class Config:
     def __init__(self, config_file):
+        if config_file == "config/config.json":
+            os.path.abspath(os.path.join(os.path.dirname(__file__), "../config", config_file))
         if os.path.exists(config_file):
             with open(config_file, "r") as f:
                 self.data = json.load(f)
         else:
-            config_default = "config/config_template.json"
+            config_default = os.path.join(os.path.dirname(__file__), "../config/config_template.json")
             if os.path.exists(config_default):
                 print(
                     f"{config_file} file not found. Using default values from {config_default}"

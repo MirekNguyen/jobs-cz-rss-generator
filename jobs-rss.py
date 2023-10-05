9a934e7dca3d6c0105323dc4b9721a1a9ef9fca3
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-10-05 21:29:55 +0200

    feat: separate feed classes

--- jobs-rss.py
+++ jobs-rss.py
@@ -1,13 +1,19 @@
+import sys
+
 import pytz
 import requests
 from bs4 import BeautifulSoup
+from feedgen.feed import FeedGenerator
 
-url = "https://www.jobs.cz/prace/praha/?q%5B%5D=PHP&date=24h&locality%5Bradius%5D=0"
+url = "https://www.jobs.cz/prace/praha/?q%5B%5D=PHP&date=3d&locality%5Bradius%5D=0"
 timezone = pytz.timezone("Europe/Prague")
 feed_id = "https://genshin-impact.fandom.com/wiki/Version"
-feed_title = "Genshin versions"
-feed_subtitle = "Genshin versions"
-feed_link_href = "https://genshin-impact.fandom.com/wiki/Version"
+feed_title = "Jobs"
+feed_subtitle = "Jobs"
+feed_link_href = (
+    "https://www.jobs.cz/prace/praha/?q%5B%5D=PHP&date=24h&locality%5Bradius%5D=0"
+)
+
 
 class JobArticle:
     def __init__(self, job_title, job_link, job_status, location, company_name):
@@ -16,6 +22,7 @@ class JobArticle:
         self.job_status = job_status
         self.location = location
         self.company_name = company_name
+
     def print(self):
         print("Job Title:", self.job_title)
         print("Job Link:", self.job_link)
@@ -37,33 +44,79 @@ class WebScrapeData:
         self.has_error = False
         response = requests.get(url)
         self.soup = BeautifulSoup(response.content, "html.parser")
-        self.target_tables = self.find_data()
+        self.data = self.find_data()
+
 
 class WebScrapeCustom(WebScrapeData):
     def find_data(self):
-        container = self.soup.find(id='search-result-container')
+        container = self.soup.find(id="search-result-container")
         if container:
-            articles = container.find_all('article', class_='SearchResultCard')
+            articles = container.find_all("article", class_="SearchResultCard")
+            jobs = []
             for article in articles:
-                self.extract_data(article)
+                job = self.extract_data(article)
+                jobs.append(job)
+            return jobs
         else:
             self.trigger_error("Element with ID 'search-result-container' not found.")
+
     def extract_data(self, article):
         if article:
             # Extract data from the <article> element
-            job_title = article.find('h2', class_='SearchResultCard__title').text.strip()
-            job_link = article.find('a', class_='SearchResultCard__titleLink')['href']
-            job_status = article.find('div', class_='SearchResultCard__status').text.strip()
-            location = article.find('li', class_='SearchResultCard__footerItem',attrs={'data-test': 'serp-locality'}).text.strip()
-            company_name = article.find('li', class_='SearchResultCard__footerItem').text.strip()
+            job_title = article.find(
+                "h2", class_="SearchResultCard__title"
+            ).text.strip()
+            job_link = article.find("a", class_="SearchResultCard__titleLink")["href"]
+            job_status = article.find(
+                "div", class_="SearchResultCard__status"
+            ).text.strip()
+            location = article.find(
+                "li",
+                class_="SearchResultCard__footerItem",
+                attrs={"data-test": "serp-locality"},
+            ).text.strip()
+            company_name = article.find(
+                "li", class_="SearchResultCard__footerItem"
+            ).text.strip()
             if job_title and job_link and job_status and location and company_name:
-                job = JobArticle(job_title, job_link, job_status, location, company_name)
-                job.print()
+                job = JobArticle(
+                    job_title, job_link, job_status, location, company_name
+                )
+                return job
             else:
                 self.trigger_error("Article attribute was not found.")
         else:
             self.trigger_error("Article was not found.")
-        
+
+
+class Feed:
+    def __init__(self):
+        self.fg = FeedGenerator()
+        self.fg.id(feed_id)
+        self.fg.title(feed_title)
+        self.fg.subtitle(feed_subtitle)
+        self.fg.link(href=feed_link_href, rel="self")
+        self.fg.language("en")
+        pass
+
+class FeedCustom(Feed):
+    def __init__(self, data):
+        super().__init__()
+        for item in data.data:
+            fe = self.fg.add_entry()
+            fe.id(item.job_title)
+            fe.title(item.job_status + ' | ' + item.company_name + ' | ' + item.job_title)
+            fe.link(href=item.job_link, replace=True)
+            fe.description(
+                "Title: " + item.job_title + "<br>" +
+                "Status: " + item.job_status + "<br>" +
+                "Location: " + item.location + "<br>" +
+                "Company: " + item.company_name
+            )
+        self.fg.rss_str(pretty=True)
+        self.fg.rss_file('./jobs-rss.xml')
+
+
 
 jobs = WebScrapeCustom(url)
-jobs.find_data()
+feed = FeedCustom(jobs)

86ea4ff1e47c77552cbacad4e026b9d98cbcb789
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-10-06 02:06:16 +0200

    fix: config file instead of directory

--- classes/config.py
+++ classes/config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     def __init__(self, config_file):
         if config_file == "config/config.json":
-            config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../config"))
+            config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../config/config.json"))
         if os.path.exists(config_file):
             with open(config_file, "r") as f:
                 self.data = json.load(f)
